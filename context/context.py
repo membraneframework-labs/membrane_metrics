dc413116af7f3a7d1250ddc19e06bc424c48ac60
Wojciech Barczynski <barczynski.w@gmail.com>
2023-03-27 11:27:45 +0200

    Add custom end date handling

--- context/context.py
+++ context/context.py
@@ -14,18 +14,10 @@ class Context:
     def __init__(self):
         with open('config.toml', "rb") as file:
             config = tomli.load(file)
-        self.start_date = Context.get_start_date(config)
-        self.end_date = date.today()
-        self.mongodb_connection_url = Context.get_mongodb_connection_url(config)
+        self.start_date = config['general']['start_date']
+        self.end_date = config['general'].get('end_date', date.today())
+        self.mongodb_connection_url = config['mongodb']['url']
         self.discord_context = Context.get_discord_context(config)
-
-    @staticmethod
-    def get_start_date(config: dict) -> date:
-        return date.fromisoformat(config['general']['start_date'])
-
-    @staticmethod
-    def get_mongodb_connection_url(config: dict) -> str:
-        return config['mongodb']['url']
 
     @staticmethod
     def get_discord_context(config: dict) -> DiscordContext:

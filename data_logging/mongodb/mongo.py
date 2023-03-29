e474c13daef6886c51dfb45a93edb45d436d86c3
Wojciech Barczynski <barczynski.w@gmail.com>
2023-03-29 15:06:36 +0200

    Fix time series filtering

--- data_logging/mongodb/mongo.py
+++ data_logging/mongodb/mongo.py
@@ -15,14 +15,14 @@ class MongoDB:
         self.db = self.client[database_name]
 
     def write_time_series(self, time_series: TimeSeries) -> None:
-        collection = self.db[time_series.collection.name]
-        collection_time_stamps = {
-            document['date'] for document in collection.find({})}
+        db_collection = self.db[time_series.collection.name]
+        collection_time_series = {
+            (document['date'], document.get(time_series.collection.get_meta_field_name(), None)) for document in db_collection.find({})}
         filtered_time_series = TimeSeries(
             time_series.collection,
             list(filter(
-                lambda data_point: data_point.date not in collection_time_stamps, time_series.values))
+                lambda data_point: (data_point.date, data_point.meta_field_value) not in collection_time_series, time_series.values))
         )
         to_insert = filtered_time_series.to_mongo_format()
         if len(to_insert) > 0:
-            collection.insert_many(to_insert)
+            db_collection.insert_many(to_insert)

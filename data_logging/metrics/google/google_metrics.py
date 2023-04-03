90b4dd5cbd7818764bae71f65e6b2388d56589ff
varsill <lukasz.kita0@gmail.com>
2023-04-03 16:07:20 +0200

    perform formatting

--- data_logging/metrics/google/google_metrics.py
+++ data_logging/metrics/google/google_metrics.py
@@ -26,23 +26,23 @@ class GoogleMetrics(Metrics):
         self.bounce_rate_per_day = self.__get_bounce_rate_per_day()
 
     def get_metric_series(self) -> list[TimeSeries]:
-        return (
-            [
-                TimeSeries(
-                    MongoCollection.GoogleTimeSpentPerDay,
-                    self.time_spent_per_day_entries,
-                ),
-                TimeSeries(
-                    MongoCollection.GoogleBounceRatePerDay, self.bounce_rate_per_day
-                ),
-                TimeSeries(
-                        MongoCollection.GoogleUniqueUsersInTutorialPerDay, self.unique_users_in_tutorials_per_day
-                    ),
-                TimeSeries(
-                        MongoCollection.GoogleUsersFromTrafficSourcePerDay, self.unique_users_from_traffic_source_per_day
-                    )
-            ]
-        )
+        return [
+            TimeSeries(
+                MongoCollection.GoogleTimeSpentPerDay,
+                self.time_spent_per_day_entries,
+            ),
+            TimeSeries(
+                MongoCollection.GoogleBounceRatePerDay, self.bounce_rate_per_day
+            ),
+            TimeSeries(
+                MongoCollection.GoogleUniqueUsersInTutorialPerDay,
+                self.unique_users_in_tutorials_per_day,
+            ),
+            TimeSeries(
+                MongoCollection.GoogleUsersFromTrafficSourcePerDay,
+                self.unique_users_from_traffic_source_per_day,
+            ),
+        ]
 
     def __get_time_spent_per_day(self) -> List[TimeSeriesEntry]:
         total_time_spent_per_day = GoogleAPI.get_total_time_spent(
@@ -75,7 +75,7 @@ class GoogleMetrics(Metrics):
         number_of_users_from_source_pers_day = GoogleAPI.get_users_source(
             GoogleMetrics.property_id, self.start_date, self.end_date
         )
-        
+
         cumulated_entries = []
         for source in number_of_users_from_source_pers_day.keys():
             entries = [

513e5fa40b5369da20a3397e82b3d4f218ebf3a2
varsill <lukasz.kita0@gmail.com>
2023-04-18 12:35:59 +0200

    Reformat the code

--- config/app_config.py
+++ config/app_config.py
@@ -6,6 +6,7 @@ from config.discord_config import DiscordConfig, BotToken, GuildID
 from config.google_config import GoogleConfig
 from config.plots_config import PlotsConfig
 
+
 class AppConfig:
     start_date: date
     end_date: date
@@ -22,14 +23,16 @@ class AppConfig:
         self.mongodb_connection_url = config["mongodb"]["url"]
         self.discord_config = AppConfig.get_discord_config(config)
         self.google_config = AppConfig.get_google_config(config)
-        self.plots_config = PlotsConfig(config["plots"]["username"], config["plots"]["password"])
+        self.plots_config = PlotsConfig(
+            config["plots"]["username"], config["plots"]["password"]
+        )
 
     @staticmethod
     def get_discord_config(config: dict) -> DiscordConfig:
         bot_token = BotToken(config["discord"]["bot_token"])
         guild_id = GuildID(config["discord"]["guild_id"])
         return DiscordConfig(bot_token, guild_id)
-    
+
     @staticmethod
     def get_google_config(config: dict) -> GoogleConfig:
         path_to_secrets_file = config["google"]["secrets_file_path"]
--- config/plots_config.py
+++ config/plots_config.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class PlotsConfig:
     username: str

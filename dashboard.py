19fe87bcdb1bb1c340035737fe00eff7378ad3cf
varsill <lukasz.kita0@gmail.com>
2023-04-24 11:35:40 +0200

    Make the authentication work will the subpages

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,20 @@ from dash import Dash, html
 
 from config.app_config import AppConfig
 
+# Monkey patch basic auth to work on non-index pages
+def basic_auth_wrapper(basic_auth, func):
+    """Updated auth wrapper to work on all pages rather than just index"""
+
+    def wrap(*args, **kwargs):
+        if basic_auth.is_authorized():
+            return func(*args, **kwargs)
+        return basic_auth.login_request()
+
+    return wrap
+
+
+dash_auth.BasicAuth.auth_wrapper = basic_auth_wrapper
+
 dash_app = Dash(__name__, use_pages=True)
 dash_app.layout = html.Div([dash.page_container])
 config = AppConfig()
